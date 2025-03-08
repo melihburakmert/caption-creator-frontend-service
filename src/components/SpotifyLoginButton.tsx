1229d5baa877cb90a275b0d4acaaee8d1070a1af
melihburakmert <melih.burak.mert@gmail.com>
2025-03-08 22:57:33 +0100

    Add spotify login redirect button

---
+++ src/components/SpotifyLoginButton.tsx
@@ -0,0 +1,24 @@
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const SPOTIFY_REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+const SPOTIFY_AUTH_ENDPOINT = import.meta.env.VITE_SPOTIFY_AUTH_ENDPOINT;
+
+const scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played'].join(' ');
+
+export const SpotifyLoginButton = ({ sessionId }: { sessionId: string }) => {
+  const getAuthUrl = () => {
+    return (
+      `${SPOTIFY_AUTH_ENDPOINT}` +
+      `?client_id=${SPOTIFY_CLIENT_ID}` +
+      `&response_type=code` +
+      `&redirect_uri=${encodeURIComponent(SPOTIFY_REDIRECT_URI)}` +
+      `&scope=${encodeURIComponent(scopes)}` +
+      `&state=${sessionId}`
+    );
+  };
+
+  return (
+    <div>
+      <a href={getAuthUrl()}>Login with Spotify</a>
+    </div>
+  );
+};
