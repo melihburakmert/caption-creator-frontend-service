d2fd827f89e120516f96b8e837b95dc4554172c9
melihburakmert <melih.burak.mert@gmail.com>
2025-03-11 00:34:27 +0100

    Add image upload logic

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, useEffect, useMemo, useState } from 'react';
 
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
-import { SpotifyLoginButton } from '@/components/SpotifyLoginButton';
+import { ParentBox, SpotifyLoginButton } from '@/components/.';
 import { QueryFactory } from '@/utils/query/QueryFactory';
 
 export const MainView = () => {
@@ -20,11 +20,12 @@ export const MainView = () => {
     return newSessionId;
   }, []);
 
+  // Recently played songs
+  const spotifyToken = localStorage.getItem('spotify_token');
+
   const [recentlyPlayedFetched, setRecentlyPlayedFetched] = useState(
     () => localStorage.getItem('recently_played_fetched') === 'true',
   );
-
-  const spotifyToken = localStorage.getItem('spotify_token');
 
   const { status: recentlyPlayedStatus, error: recentlyPlayedError } = useQuery({
     ...QueryFactory.getRecentlyPlayed(sessionId),
@@ -56,9 +57,51 @@ export const MainView = () => {
     localStorage.setItem('recently_played_fetched', recentlyPlayedFetched.toString());
   }, [recentlyPlayedFetched]);
 
+  // Image
+  const [image, setImage] = useState<string | null>(() => {
+    return localStorage.getItem('uploadedImage');
+  });
+
+  const { mutate: getImageDescription } = useMutation({
+    mutationFn: (form: FormData) => QueryFactory.getImageDescription(sessionId).mutationFn(form),
+    onSuccess: () => {},
+    onError: error => {
+      console.error('Error uploading the image: ', error);
+    },
+  });
+
+  const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64Image = reader.result as string;
+        setImage(base64Image);
+        localStorage.setItem('uploadedImage', base64Image);
+      };
+      reader.readAsDataURL(file);
+
+      const formData = new FormData();
+      formData.append('image', file);
+
+      getImageDescription(formData);
+    }
+  };
+
+  useEffect(() => {
+    const storedImage = localStorage.getItem('uploadedImage');
+    if (storedImage) {
+      setImage(storedImage);
+    }
+  }, []);
+
+  // Caption
+  const [caption, setCaption] = useState<string>('');
+
   return (
     <div>
-      <h1>Main View</h1>
+      <ParentBox image={image} caption={caption} handleImageUpload={handleImageUpload} />
       {!spotifyToken && <SpotifyLoginButton sessionId={sessionId} />}
       {spotifyToken && !recentlyPlayedFetched && recentlyPlayedStatus === 'pending' && (
         <p>Fetching recently played songs...</p>
